f09d352465e8182d3765b3f67fbc7d00697072d8
Danilo Silva <contact@danilocgsilva.me>
2025-10-03 15:30:44 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Make Question Interface
+
+This is an interface package that will drives the implementation of client and server package that will make questions to a IA.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="make_question_interface",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Danilo Silva",
+    author_email="contact@danilocgsilva.me",
+    description="Drives client and server code to implement a functionality that makes question to a IA",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/my_package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
