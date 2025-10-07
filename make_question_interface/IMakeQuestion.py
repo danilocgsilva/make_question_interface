cfe149a9a7205b9e5749f01fa8f5c636ea3d8a3b
Danilo Silva <danilocgsilva@gmail.com>
2025-10-07 11:20:50 -0300

    Basic methods seted

--- make_question_interface/IMakeQuestion.py
+++ make_question_interface/IMakeQuestion.py
@@ -1,4 +1,8 @@
 import abc
 
 class IMakeQuestion(abc.ABC):
+    def make_question(self, question: str):
+        pass
     
+    def get_answer_text_raw(self) -> str:
+        pass
--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="make_question_interface",
-    version="0.1.0",
+    version="1.0.0",
     packages=find_packages(),
     author="Danilo Silva",
     author_email="contact@danilocgsilva.me",
     description="Drives client and server code to implement a functionality that makes question to a IA",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/my_package",
+    url="https://github.com/danilocgsilva/make_question_interface",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
