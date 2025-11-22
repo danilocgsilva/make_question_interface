e0bd576990569eeccbd4ff055f7a4e7e9ecd2e63
Danilo Silva <contact@danilocgsilva.me>
2025-11-22 19:17:02 -0300

    Saves haw model response

--- make_question_interface/Results.py
+++ make_question_interface/Results.py
@@ -1,21 +1,24 @@
 from dataclasses import dataclass
+import pickle
+import base64
 
 @dataclass
 class Results:
-    raw_answer: str
+    raw_answer_serialized: str
     timestamp_start: float
     timestamp_end: float
     implementation_name: str
     model_name: str
+    parameters: dict
     
     def __init__(
         self, 
-        raw_answer: str, 
+        raw_answer: any, 
         timestamp_start: float, 
         timestamp_end: float,
         implementation_name: str,
         model_name: str,
-        parameters: dict = None
+        parameters: dict = None,
     ):
         self.raw_answer = raw_answer
         self.timestamp_start = timestamp_start
@@ -23,3 +26,4 @@ class Results:
         self.implementation_name = implementation_name
         self.model_name = model_name
         self.parameters = parameters if parameters is not None else {}
+        self.raw_answer_serialized = base64.b64encode(pickle.dumps(raw_answer)).decode('utf-8')
